1b85e6cbfcf35c40dddae76693deff791bf197fb
paledust <paledust@hotmail.com>
2024-11-19 16:28:56 +0800

    make circle float

--- Assets/Scripts/Motion/FloatingMotion.cs
+++ Assets/Scripts/Motion/FloatingMotion.cs
@@ -7,8 +7,6 @@ public class FloatingMotion : MonoBehaviour
     public float floatFreq = 1;
     public float floatHeight = 2;
     public float floatOffset = 1;
-    [SerializeField] private bool snappyFloat = false;
-    [SerializeField] private float snappyStep = 1;
 [Header("Noise")]
     [SerializeField] private float noiseScale = 0.5f;
     [SerializeField] private float noiseFreq = 1;
@@ -26,14 +24,6 @@ public class FloatingMotion : MonoBehaviour
         float noise = Mathf.PerlinNoise(noiseFreq*(Time.time+seed),noiseFreq*(Time.time+seed));
         noise = (noise*2 - 1)*noiseScale;
 
-        if(snappyFloat){
-            Vector3 pos = Vector3.up * floatHeight * (Mathf.Sin(timer + seed*Mathf.PI)+noise) + Vector3.right * floatOffset * Mathf.Cos(timer*0.7f + seed*Mathf.PI) + initPos;
-            pos.x = Mathf.Floor(pos.x*snappyStep)/snappyStep;
-            pos.y = Mathf.Floor(pos.y*snappyStep)/snappyStep;
-            pos.z = Mathf.Floor(pos.z*snappyStep)/snappyStep;
-            transform.localPosition = pos;
-        }
-        else
-            transform.localPosition = Vector3.up * floatHeight * (Mathf.Sin(timer + seed*Mathf.PI)+noise) + Vector3.right * floatOffset * Mathf.Cos(timer*0.7f + seed*Mathf.PI) + initPos;
+        transform.localPosition = Vector3.up * floatHeight * (Mathf.Sin(timer + seed*Mathf.PI)+noise) + Vector3.right * floatOffset * Mathf.Cos(timer*0.7f + seed*Mathf.PI) + initPos;
     }
 }
