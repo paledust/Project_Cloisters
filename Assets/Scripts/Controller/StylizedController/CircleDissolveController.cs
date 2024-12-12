77650bbfb81c828b49eee621f7dfc0f2adff21ab
paledust <paledust@hotmail.com>
2024-12-12 22:56:49 +0800

    can shrink and expand circle

--- Assets/Scripts/Controller/StylizedController/CircleDissolveController.cs
+++ Assets/Scripts/Controller/StylizedController/CircleDissolveController.cs
@@ -9,19 +9,28 @@ public class CircleDissolveController : MonoBehaviour
     [SerializeField] private PerRendererExpand expandCircle;
     [SerializeField] private float dissolveMaxAngle;
     [SerializeField] private float dissolveRadius;
-
+    [SerializeField] private float shrinkRadius;
+    
     private float initAngle;
     private float initRadius;
+    private float dissolveMinAngle;
 
     public void ResetController(){
-        initAngle = clickable_Planet.m_accumulateYaw-clickable_Planet.m_angularSpeed*1;
+        dissolveMinAngle = clickable_Planet.m_angularSpeed*0f;
+        initAngle = clickable_Planet.m_accumulateYaw;
         initRadius = expandCircle.circleRadius;
     }
     void Update()
     {
-        float offset = (initAngle - clickable_Planet.m_accumulateYaw)/dissolveMaxAngle;
+        float angleOffset = -(clickable_Planet.m_accumulateYaw-initAngle);
+        float offset = (Mathf.Abs(angleOffset)-dissolveMinAngle)/(dissolveMaxAngle-dissolveMinAngle);
         offset = Mathf.Clamp01(offset);
-        expandCircle.circleRadius = Mathf.Lerp(initRadius, dissolveRadius, offset);
+
+        if(Mathf.Sign(angleOffset)>0)
+            expandCircle.circleRadius = Mathf.Lerp(initRadius, dissolveRadius, EasingFunc.Easing.QuadEaseIn(offset));
+        else
+            expandCircle.circleRadius = Mathf.Lerp(initRadius, shrinkRadius, EasingFunc.Easing.QuadEaseIn(offset));
+
         if(offset>=1){
             iC_Stylized.StartExpand();
         }
