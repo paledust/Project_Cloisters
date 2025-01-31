df0d16b656f339ccc08ae4b984dc670ab5915b1f
paledust <paledust@hotmail.com>
2025-01-31 23:38:21 +0800

    update crystal

--- Assets/Scripts/Controller/WhimsicalController/WhimsicalRingController.cs
+++ Assets/Scripts/Controller/WhimsicalController/WhimsicalRingController.cs
@@ -20,14 +20,16 @@ public class WhimsicalRingController : MonoBehaviour
         {
             ring.transform.localScale += Vector3.one * speedToExpand * crystal.m_angularSpeed * Time.deltaTime;
 
-            if(ring.transform.localScale.x > ringScaleRange.y)
+            if(ring.transform.localScale.x > 2*ringScaleRange.y-ringScaleRange.x)
             {
+                ring.RefreshParticle();
                 ring.transform.localScale = ringScaleRange.x * Vector3.one;
                 ring.transform.rotation = Quaternion.Euler(GetRndAngle(pitchRange),0,0) * Quaternion.Euler(0,0,GetRndAngle(rollRange));
             }
             else if(ring.transform.localScale.x < ringScaleRange.x)
             {
-                ring.transform.localScale = ringScaleRange.y * Vector3.one;
+                ring.RefreshParticle();
+                ring.transform.localScale = (2*ringScaleRange.y-ringScaleRange.x) * Vector3.one;
                 ring.transform.rotation = Quaternion.Euler(GetRndAngle(pitchRange),0,0) * Quaternion.Euler(0,0,GetRndAngle(rollRange));
             }
             ring.UpdateRingColor(opacityCurve.Evaluate(Mathf.InverseLerp(ringScaleRange.x, ringScaleRange.y, ring.transform.localScale.x)));
--- Assets/Scripts/Others/Whimsical/CrystalRing.cs
+++ Assets/Scripts/Others/Whimsical/CrystalRing.cs
@@ -6,9 +6,21 @@ public class CrystalRing : MonoBehaviour
 {
     [SerializeField] private PerRendererOpacity crystalOpacity;
     [SerializeField] private PerRendererColor ringColor;
+[Header("Crystal Ring")]
+    [SerializeField] private ParticleSystem p_crystal;
+    [SerializeField] private float rotationSpeed = 20;
     public void UpdateRingColor(float opacity)
     {
         crystalOpacity.opacity = opacity;
         ringColor.tint.a = opacity;
+    }
+    void Update()
+    {
+        p_crystal.transform.Rotate(0,0,rotationSpeed*Time.deltaTime,Space.Self);
+    }
+    public void RefreshParticle()
+    {
+        p_crystal.Clear();
+        p_crystal.Play();
     }
 }
