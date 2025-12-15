fe790b72f79c0e9d59fe3ba11cd929ce0d92701d
paledust <paledust@hotmail.com>
2025-12-15 23:34:35 +0800

    update cloisters interaction

--- Assets/Scripts/Clickable/Clickable_CloisterSphere.cs
+++ Assets/Scripts/Clickable/Clickable_CloisterSphere.cs
@@ -1,8 +1,64 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Clickable_CloisterSphere : MonoBehaviour
+public class Clickable_CloisterSphere : Basic_Clickable
 {
+    [SerializeField] private Transform rotateTrans;
+[Header("Control")]
+    [SerializeField] private float dragStrength = 1f;
+    [SerializeField] private float maxAngularSpeed = 200f;
+[Header("Resize")]
+    [SerializeField] private float resizeFactor = 1.02f;
+    [SerializeField] private float resizeTime = 0.4f;
+    [SerializeField] private float backTime = 0.5f;
+[Header("AngularSpeed Lerp")]
+    [SerializeField] private float controllingAngularLerp = 10f;
+    [SerializeField] private float releaseAngularLerp = 1f;
 
+    public float m_angularSpeed{get; private set;}
+
+    private PlayerController playerController;
+    private CoroutineExcuter sizeChanger;
+
+    void Start()
+    {
+        sizeChanger = new CoroutineExcuter(this);
+    }
+    void Update()
+    {
+        if(playerController!=null){
+            Vector2 delta = playerController.PointerDelta;
+            m_angularSpeed = Mathf.Lerp(m_angularSpeed, Mathf.Clamp(delta.x * dragStrength, -maxAngularSpeed, maxAngularSpeed), Time.deltaTime*controllingAngularLerp);
+        }
+        else{
+            m_angularSpeed = Mathf.Lerp(m_angularSpeed, 0, Time.deltaTime*releaseAngularLerp);
+            if(Mathf.Abs(m_angularSpeed)<=0.01f) m_angularSpeed = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        rotateTrans.Rotate(Vector3.up, -m_angularSpeed*Time.fixedDeltaTime, Space.Self);
+    }
+    public override void OnClick(PlayerController player, Vector3 hitPos)
+    {
+        base.OnClick(player, hitPos);
+        playerController = player;
+        playerController.HoldInteractable(this);
+
+        sizeChanger.Excute(coroutineChangePlanetSize(resizeFactor, resizeTime, EasingFunc.Easing.FunctionType.BackEaseOut));
+    }
+    public override void OnRelease(PlayerController player)
+    {
+        base.OnRelease(player);
+        playerController = null;
+        sizeChanger.Excute(coroutineChangePlanetSize(1f, backTime));
+    }
+    IEnumerator coroutineChangePlanetSize(float targetSize, float duration, EasingFunc.Easing.FunctionType easeType = EasingFunc.Easing.FunctionType.QuadEaseOut){
+        var easeFunc = EasingFunc.Easing.GetFunctionWithTypeEnum(easeType);
+        Vector3 initSize = rotateTrans.localScale;
+
+        yield return new WaitForLoop(duration, (t)=>{
+            rotateTrans.localScale = Vector3.LerpUnclamped(initSize, targetSize*Vector3.one, easeFunc(t));
+        });
+    }
 }
--- Assets/Scripts/Interaction/InteractionControllers/IC_Cloisters.cs
+++ Assets/Scripts/Interaction/InteractionControllers/IC_Cloisters.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class IC_Cloisters : IC_Basic
 {
-    [SerializeField] private Clickable_ObjectRotator rotator;
+    [SerializeField] private Clickable_CloisterSphere heroSphere;
+    
+    [Header("Main Feedback")]
+    [SerializeField] private Animator shineDissolve;
     [SerializeField] private float threasholdRotatorSpeed;
-    [SerializeField] private float grawingSpeed = 1f;
     [SerializeField] private float switchAmount = 0.5f;
+    [SerializeField] private float grawingSpeed = 1f;
+    
+    [Header("Totem")]
+    [SerializeField] private PerRendererCloistersDissolve constructDissolve;
+    [SerializeField] private PerRendererCloistersDissolve plantDissolve;
+    [SerializeField] private PerRendererCloistersDissolve textDissolve;
+    [SerializeField] private PerRendererCloistersDissolve sunDissolve;
+    [SerializeField, ShowOnly] private float progress;
+
     private bool isDay = true;
-    private float progress;
+    private bool isDissolveIn = false;
+    private const string DISSOLVE_IN_BOOLEAN = "DissolveIn";
 
     protected void Update()
     {
-        if(rotator.m_angularSpeed > threasholdRotatorSpeed)
+        if(heroSphere.m_angularSpeed > threasholdRotatorSpeed)
         {
             progress += Time.deltaTime * grawingSpeed;
+            if(!isDissolveIn)
+            {
+                isDissolveIn = true;
+                shineDissolve.SetBool(DISSOLVE_IN_BOOLEAN, isDissolveIn);
+            }
+        }
+        else
+        {
+            if(isDissolveIn)
+            {
+                isDissolveIn = false;
+                shineDissolve.SetBool(DISSOLVE_IN_BOOLEAN, isDissolveIn);
+            }
         }
         if(isDay && progress >= switchAmount)
         {
