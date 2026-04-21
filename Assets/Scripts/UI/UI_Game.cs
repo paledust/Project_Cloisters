2a4df2fe68fab2b28c241785a2ca29a090d95696
paledust <paledust@hotmail.com>
2026-04-22 00:50:38 +0800

    tune menu

--- Assets/Scripts/UI/UI_Game.cs
+++ Assets/Scripts/UI/UI_Game.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using SimpleAudioSystem;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class UI_Game : MonoBehaviour
@@ -9,6 +10,8 @@ public class UI_Game : MonoBehaviour
     [SerializeField] private GraphicRaycaster raycaster;
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private bool isMenuOpen = false;
+    [SerializeField] private float menuTime = .3f;
+    [SerializeField] private Volume menuVolume;
 
 [Header("BlackBar")]
     [SerializeField] private Image blackBar_Top;
@@ -26,26 +29,32 @@ public class UI_Game : MonoBehaviour
     {
         isMenuOpen = true;
         canvasGroup.DOKill();
-        canvasGroup.DOFade(1, 0.1f).OnComplete(()=>canvasGroup.interactable = true);
+        canvasGroup.DOFade(1, menuTime).OnComplete(()=>canvasGroup.interactable = true);
 
         blackBar_Top.DOKill();
         blackBar_Bottom.DOKill();
         blackBar_Top.rectTransform.anchoredPosition = new Vector2(0, 100);
         blackBar_Bottom.rectTransform.anchoredPosition = new Vector2(0, -100);
-        blackBar_Top.rectTransform.DOAnchorPosY(0, 0.1f).SetEase(Ease.OutQuad);
-        blackBar_Bottom.rectTransform.DOAnchorPosY(0, 0.1f).SetEase(Ease.OutQuad);
+        blackBar_Top.rectTransform.DOAnchorPosY(0, menuTime).SetEase(Ease.OutQuad);
+        blackBar_Bottom.rectTransform.DOAnchorPosY(0, menuTime).SetEase(Ease.OutQuad);
+
+        menuVolume.DOKill();
+        DOTween.To(() => menuVolume.weight, x => menuVolume.weight = x, 1, menuTime).SetEase(Ease.OutQuad);
     }
     public void DisableCanvas()
     {
         isMenuOpen = false;
         canvasGroup.interactable = false;
         canvasGroup.DOKill();
-        canvasGroup.DOFade(0, 0.1f);
+        canvasGroup.DOFade(0, menuTime);
 
         blackBar_Top.DOKill();
         blackBar_Bottom.DOKill();
-        blackBar_Top.rectTransform.DOAnchorPosY(100, 0.1f).SetEase(Ease.OutQuad);
-        blackBar_Bottom.rectTransform.DOAnchorPosY(-100, 0.1f).SetEase(Ease.OutQuad);
+        blackBar_Top.rectTransform.DOAnchorPosY(100, menuTime).SetEase(Ease.OutQuad);
+        blackBar_Bottom.rectTransform.DOAnchorPosY(-100, menuTime).SetEase(Ease.OutQuad);
+
+        menuVolume.DOKill();
+        DOTween.To(() => menuVolume.weight, x => menuVolume.weight = x, 0, menuTime).SetEase(Ease.OutQuad);
     }
     public void Btn_RestartGame()
     {
