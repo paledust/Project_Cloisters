3c92248338028c0abdaf45f106f296eb28b23342
paledust <paledust@hotmail.com>
2026-06-27 16:03:00 +0800

    create replacement assets

---
+++ Assets/DevelopBasic/SimpleAudioSystem/Editor/AudioDataCollectionInspector.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace SimpleAudioSystem
+{
+    [CustomEditor(typeof(AudioDataCollection_SO))]
+    public class AudioDataCollectionInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
