8861cd493aa2ef7e8f51528b5bab948a7c280327
paledust <paledust@hotmail.com>
2026-07-19 23:22:52 +0800

    update sfx in evocative

--- Assets/Scripts/Others/Evocative/RotateBar.cs
+++ Assets/Scripts/Others/Evocative/RotateBar.cs
@@ -6,17 +6,19 @@ public class RotateBar : MonoBehaviour
 {
     [SerializeField] private float rotateSpeedMulti = 10;
     [SerializeField] private Transform rotateTarget;
+    [SerializeField] private ParticleSystem vfx_splat;
 
     [Header("Audio")]
-    [SerializeField] private string sfxFlip;
+    [SerializeField] private AudioData_SO sfxFlip;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out BounceBall ball))
         {
-            AudioManager.Instance.PlaySFX(sfxFlip, 1);
+            AudioManager.Instance.PlaySFX(sfxFlip.AudioKey, 1);
             float speed = ball.m_currentSpeed * rotateSpeedMulti;
             float angle = rotateTarget.localEulerAngles.z + 360*5;
+            vfx_splat.Play();
             rotateTarget.DOKill();
             rotateTarget.DORotate(new Vector3(0, 0, angle), 360*5/speed, RotateMode.FastBeyond360).SetEase(Ease.OutQuad);
         }
